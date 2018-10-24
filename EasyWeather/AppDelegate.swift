ded72b4a95b237949cab717dd0d4b59a66410e46
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-10-24 12:40:59 +0200

    CMP scenario updated.
    IDFA is cached & compared

--- EasyWeather/AppDelegate.swift
+++ EasyWeather/AppDelegate.swift
@@ -26,12 +26,13 @@ import Firebase
 import Fabric
 import Crashlytics
 import HyBid
+import AdSupport.ASIdentifierManager
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         Fabric.with([Crashlytics.self])
@@ -48,41 +49,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     {
         if !HyBidUserDataManager.sharedInstance().canCollectData() {
             if(UserDefaults.standard.object(forKey: "ConsentCheckDate") == nil) {
-                HyBidUserDataManager.sharedInstance().showConsentRequestScreen()
-                UserDefaults.standard.set(Date(), forKey: "ConsentCheckDate")
+                showConsentAndSaveParameters()
             } else {
-                let consentCheckDate = UserDefaults.standard.object(forKey: "ConsentCheckDate") as? Date
-                let components = Calendar.current.dateComponents([.minute, .hour, .day, .month, .year], from: consentCheckDate!, to: Date())
-                if (components.day! > 31) {
-                    HyBidUserDataManager.sharedInstance().showConsentRequestScreen()
-                    UserDefaults.standard.set(Date(), forKey: "ConsentCheckDate")
+                if(UserDefaults.standard.object(forKey: "IDFA") != nil) {
+                    let idfa = UserDefaults.standard.object(forKey: "IDFA") as! String
+                    if (idfa == HyBidSettings.sharedInstance()?.advertisingId){
+                        let consentCheckDate = UserDefaults.standard.object(forKey: "ConsentCheckDate") as? Date
+                        let components = Calendar.current.dateComponents([.minute, .hour, .day, .month, .year], from: consentCheckDate!, to: Date())
+                        if (components.day! > 31) {
+                            showConsentAndSaveParameters()
+                        }
+                    } else {
+                        showConsentAndSaveParameters()
+                    }
                 }
             }
         }
     }
-
+    
+    
+    func showConsentAndSaveParameters()
+    {
+        HyBidUserDataManager.sharedInstance().showConsentRequestScreen()
+        UserDefaults.standard.set(Date(), forKey: "ConsentCheckDate")
+        UserDefaults.standard.set(identifierForAdvertising(), forKey: "IDFA")
+    }
+    
+    func identifierForAdvertising() -> String?
+    {
+        guard ASIdentifierManager.shared().isAdvertisingTrackingEnabled else {
+            return nil
+        }
+        return ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
     }
-
+    
 }
 
